c9a27fb2401b05e136c05d73b8ea76930af1504e
Thiago Paiva <10606829+paiva-thiago@users.noreply.github.com>
2019-10-09 08:58:24 -0300

    Java & Ruby

---
+++ HelloWorld-paiva-thiago.java
@@ -0,0 +1,6 @@
+class HelloWorldPaivaThiago {
+  public static void main(String[] args) {
+    System.out.println("Oi, divisão da Terra em seus hemisférios!");
+    System.out.println("Tá bão?");
+  }
+}
---
+++ HelloWorld-paiva-thiago.rb
@@ -0,0 +1,1 @@
+puts 'Falaê, mundão!'
